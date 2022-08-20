35c1114a9b4821c22af26d74194ef83b7dc37c82
Randall Lowe <safelock@users.noreply.github.com>
2022-08-19 20:52:32 -0700

    Create alpine-mesa-sound-dl.sh

---
+++ alpine-mesa-sound-dl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/llvm13-libs-13.0.1-r2.apk 
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-21.3.8-r2.apk
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-dri-gallium-21.3.8-r2.apk
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-egl-21.3.8-r2.apk
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-gbm-21.3.8-r2.apk
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-gl-21.3.8-r2.apk
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-glapi-21.3.8-r2.apk
+sudo wget -nd http://dl-cdn.alpinelinux.org/alpine/v3.16/main/armv7/mesa-gles-21.3.8-r2.apk
